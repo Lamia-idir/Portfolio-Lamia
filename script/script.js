f15355699ec42cddd28e1fdf31d4b67d652ff7ff
lamia <lamia.idir@outlook.fr>
2026-04-13 09:46:57 +0200

    mise à jour dossier

--- script/script.js
+++ script/script.js
@@ -72,18 +72,20 @@ function createPortfolioFromJSON() {
             // Iterate through the JSON data and create HTML elements
             data.forEach((item, index) => {
                 const card = document.createElement("div");
-                card.classList.add("col-lg-4", "mt-4");
-                card.innerHTML = `
-                    <div class="card portfolioContent">
+                    card.classList.add("col-lg-4", "mt-4");
+                    card.innerHTML = `
+                <div class="card portfolioContent">
                     <img class="card-img-top" src="images/${item.image}" style="width:100%">
                     <div class="card-body">
                         <h4 class="card-title">${item.title}</h4>
                         <p class="card-text">${item.text}</p>
                         <div class="text-center">
-                            <a href="${item.link}" class="btn btn-success">Lien</a>
+                            <a href="${item.link}" class="btn btn-success">Voir le projet</a>
                         </div>
                     </div>
+
                 </div>
+                
                 `;
 
                 // Append the card to the current row
